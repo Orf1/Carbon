5ed77bf542167ed1a6b2974c12146e48bebc93a7
Orf1 <orfeas126@gmail.com>
2021-01-09 14:52:57 -0800

    Added some extra methods to Carbon

--- src/main/java/dev/orf1/carbon/Carbon.java
+++ src/main/java/dev/orf1/carbon/Carbon.java
@@ -1,22 +1,47 @@
 package dev.orf1.carbon;
 
-import dev.orf1.carbon.listener.IListener;
 import dev.orf1.carbon.listener.impl.ConnectionListener;
 
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 
 public class Carbon {
 
-    public Carbon() throws IOException {
-        Server server = new Server(42791);
-        server.registerListener(new ConnectionListener());
-        server.start();
+    private static Carbon instance;
+    private Server server;
+    private Client client;
+    private final boolean isServer;
+
+    public Carbon(boolean isServer) throws IOException {
+        instance = this;
+        this.isServer = isServer;
+        if (isServer) {
+            server = new Server(42791);
+            server.registerListener(new ConnectionListener());
+            server.start();
+        } else {
+            client = new Client("192.168.1.1", 42791);
+        }
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Server getServer() {
+        return server;
+    }
+
+    public boolean isServer() {
+        return isServer;
+    }
+
+    public static Carbon get() {
+        return instance;
     }
 
     public static void main(String[] args) {
         try {
-            new Carbon();
+            new Carbon(true);
         } catch (Exception exception) {
             exception.printStackTrace();
         }
